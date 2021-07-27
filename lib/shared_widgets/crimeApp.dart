62486eeed8c1b190322f87c441c039763ebb9709
AlexMainaMunyua <alexmaina79@gmail.com>
2021-07-27 11:22:36 +0300

    feat: configuring firebase

--- lib/shared_widgets/crimeApp.dart
+++ lib/shared_widgets/crimeApp.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class CrimeApp {
   static late SharedPreferences sharedPreferences;
 
-  static String imageList = 'imageCart';
+  static String imageList = 'crimeImage';
 
   static String reportNumber = 'reportNumber';
 }
